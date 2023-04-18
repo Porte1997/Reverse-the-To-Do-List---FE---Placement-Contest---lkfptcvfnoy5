652003353b680b3076e490733b177c5cd34aae76
SHUBHAM PORTE <shubhamporte0@gmail.com>
2023-04-18 12:57:12 +0530

    fixed reversing

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,108 @@
 import React, { useState } from 'react'
 import '../styles/App.css';
 
-function ToDo() {
-  return (<tr>
-    <td>
-      <p>id</p>
-    </td>
-    <td>
-      <input />
-    </td>
-    <td>
-      <p>createdAt</p>
-    </td>
-  </tr>)
-}
-
 function App() {
   const [todos, setTodos] = useState([{
     id: 'todo1',
+    input: '',
     createdAt: '20:30',
   }, {
     id: 'todo2',
+    input: '',    
     createdAt: '18:00',
   }
   ]);
 
+function changed(e){
+  // console.log(e.target.id);
+  if(e.target.id === "todo1"){
+    if(todos[0].id === 'todo1'){
+      setTodos([
+      {
+        id: 'todo1',
+        input: e.target.value,
+        createdAt: '20:30',
+      },
+      {
+        id: 'todo2',
+        input: todos[1].input,    
+        createdAt: '18:00'
+      }
+    ])}
+    else{
+      setTodos([
+        {
+          id: 'todo2',
+          input: todos[0].input,
+          createdAt: '18:00'
+        },
+        {
+          id: 'todo1',
+          input: e.target.value,    
+          createdAt: '20:30',
+        }
+      ])
+    }
+  }
+  else{
+    if(todos[0].id === 'todo2'){
+      setTodos([
+      {
+        id: 'todo2',
+        input: e.target.value,
+        createdAt: '18:00',
+      },
+      {
+        id: 'todo1',
+        input: todos[1].input,    
+        createdAt: '20:30'
+      }
+    ])}
+    else{
+      setTodos([
+        {
+          id: 'todo1',
+          input: todos[0].input,
+          createdAt: '20:30',
+        },
+        {
+          id: 'todo2',
+          input: e.target.value,    
+          createdAt: '18:00'
+        }
+      ])
+    }
+  }
+}
+
+
+function ToDo(item, index) {
+  return (<tr key={index}>
+    <td>
+      <p>{item.id}</p>
+    </td>
+    <td>
+      <input value={item.input} id={item.id} onChange={changed}/>
+    </td>
+    <td>
+      <p>{item.createdAt}</p>
+    </td>
+  </tr>)
+}
+
+
+  function revToDo(){
+    setTodos([todos[1], todos[0]]);
+  }
+
+  
+
   return (
     <div id="main">
-      <button>Reverse</button>
+      <button onClick={revToDo}>Reverse</button>
       <table>
         <tbody>
+          { todos.map((item, index) => ToDo(item, index)) }
         </tbody>
       </table>
     </div>
